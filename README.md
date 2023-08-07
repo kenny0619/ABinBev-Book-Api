dccb454851f6a1d882cb3b693182bb54da9eeff3
kenny0619 <kehindeibukun0619@gmail.com>
2023-08-07 16:50:16 +0100

    fix: writes some instruction on the read me

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ABinBev-Book-Api
+
+install all dependencies
+
+You can add your env folder and file by creating a .env folder and a .env file in it. i already added some test configs in the config file so its not necessary
+To run the app yarn start:dev:service
+
+the router explorer (on the terminal) shows all the available endpoints.
+
+i'll update with a postman link as soon as i submit
+
--- config/configuration.ts
+++ config/configuration.ts
@@ -4,26 +4,30 @@ export const configuration = () => ({
   serviceName: process.env.SERVICE_NAME || "Service",
   environment: process.env.NODE_ENV || "development",
   encryption_key: process.env.SERVER_SECRET || "AppSecret",
-  jwt_expiration: process.env.JWT_EXPIRATION || 172800,
-  port: process.env.PORT || 7000,
+  port: process.env.PORT || 4000,
   pagination: {
     itemsPerPage: 10,
   },
   lang: "en",
 
-  rabbitMQ: process.env.RABBIT_MQ_URL || "amqp://localhost:5672",
+  rabbitMQ:
+    process.env.RABBIT_MQ_URL ||
+    "amqps://devmq:CloudAdmin_123@b-83994ec2-85bd-4e15-815e-cd2310e6fed2.mq.eu-north-1.amazonaws.com:5671",
   mongodb: {
-    url: process.env.DB_URL,
+    url:
+      process.env.DB_URL ||
+      "mongodb+srv://devprod:CloudAdmin_123@devstg.on5p0hj.mongodb.net/gns-stg?retryWrites=true&w=majority",
   },
 
   rdbms: {
     default: "postgres",
     postgres: {
-      host: process.env.POSTGRES_DB_HOST,
-      port: process.env.POSTGRES_DB_PORT,
-      name: process.env.POSTGRES_DB_NAME,
-      username: process.env.POSTGRES_DB_USERNAME,
-      password: process.env.POSTGRES_DB_PASSWORD,
+      host: process.env.POSTGRES_DB_HOST || "batyr.db.elephantsql.com",
+      port: process.env.POSTGRES_DB_PORT || 5432,
+      name: process.env.POSTGRES_DB_NAME || "drwchfre",
+      username: process.env.POSTGRES_DB_USERNAME || "drwchfre",
+      password:
+        process.env.POSTGRES_DB_PASSWORD || "DxXakdYiLVejNHtVguH2CEI6U39yw1Io",
     },
   },
 });
